b4b040b841691fcbe4eb3a0f3f8c4451f86248c0
arijustech <arijus.ka@outlook.com>
2024-12-14 15:07:48 +0200

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-ping -c 4 google.com
-echo "Dėmėsio visos particijos bus ištrintos!"
-echo "Aš neatsakau už padarinius, kurie gali ištikti jūsų sistemą! Būkite atsargūs!"
-echo "Ar jūs tikrai norite formatuoti diską? 1 taip 2 ne"
+echo "Dėmesio, visos particijos bus suformatuotos!"
+echo "Aš neatsakau už padarinius, tad būkite atsargūs!"
+echo "Ar tikrai norite suformatuoti diską? 1 - taip, 2 - ne"
 read p
 if [ "$p" = "1" ]; then
-	echo "particijos formatuojamos"
-	fi
+	echo "valia"
+ 
