12c37f67787d3d0d1c961a5f9e7e99350d21d717
arijustech <arijus.ka@outlook.com>
2024-12-14 15:18:51 +0200

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-echo "Dėmesio, visos particijos bus suformatuotos!"
-echo "Aš neatsakau už padarinius, tad būkite atsargūs!"
-echo "Ar tikrai norite suformatuoti diską? 1 - taip, 2 - ne"
-read p
-if [ "$p" = "1" ]; then
-	echo "valia"
- 
+hello
