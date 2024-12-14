eddaf3bd4a5e4d6232f4708001a0356421346e1c
arijustech <arijus.ka@outlook.com>
2024-12-14 15:53:14 +0200

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #checking if internet is available
 ping -c 4 google.com
-echo "
-Attention, all partitions will be formatted!
-I am not responsible for the consequences!
-Be careful the consequences can be huge!
-Do you still want to run this script? 1 - yes 2 - no"
+echo "Attention, all partitions will be formatted!"
+echo "I am not responsible for the consequences!"
+echo "Be careful the consequences can be huge!"
+echo "Do you still want to run this script? 1 - yes 2 - no"
 read p
 if [ "$p" = "1" ]; then
     echo "valia"
